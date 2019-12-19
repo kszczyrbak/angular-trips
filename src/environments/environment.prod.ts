a168c544dcaed9de834e2c3c78643192d77ec991
Krzysztof Szczyrbak <krzysztof.szczyrbak@pl.abb.com>
2019-12-19 12:02:18 +0100

    fixed envirnoment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
     appId: "1:884402463725:web:d55591e6c91252321a3de4",
     measurementId: "G-K94V5BTL4W"
   },
-  backendUrl: 'https://projekt-webowe-node.herokuapp.com:5000'
+  backendUrl: 'https://projekt-webowe-node.herokuapp.com'
 };
